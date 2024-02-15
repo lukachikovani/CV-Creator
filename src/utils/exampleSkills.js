681b3c4b7d203c8983c9c974dd92043d9f44470f
giorgi chikovani <gchikovani16@gmail.com>
2024-02-15 16:04:01 +0400

    Initial Commit

---
+++ src/utils/exampleSkills.js
@@ -0,0 +1,12 @@
+const exampleSkills = [" React", 
+                       " Node.js", 
+                       " PostgreSQL", 
+                       " Photoshop", 
+                       " SEO Optimization", 
+                       " Express.js", 
+                       " Test-Driven-Development", 
+                       " Continous Integration",
+                       " Git Workflow"
+                       ];
+
+export default exampleSkills;
